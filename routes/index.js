8fa13cb4bb8f6b8c51546426a9e5d54dba69c9c7
Stephen Pino <spino725@gmail.com>
2017-05-11 20:19:36 -0500

    adding api scripts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var authRoutes = require('./auth-routes');
 var smsRoutes = require('./sms-routes');
 var eventRoutes = require('./event-routes');
+apiRoutes = require('./api-routes');
 
 module.exports = {
     authRoutes: authRoutes,
     smsRoutes: smsRoutes,
-    eventRoutes: eventRoutes
+    eventRoutes: eventRoutes,
+    apiRoutes: apiRoutes
 }
